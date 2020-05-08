94d5eb7dbde2e78679674c9a37a85bbb9f3bd6df
gabrik <gabriele.baldoni@gmail.com>
2020-05-08 12:49:08 +0200

    added hostapd commands
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- create_ap.sh
+++ create_ap.sh
@@ -9,22 +9,22 @@ sudo ip l set up dev br0
 
 sudo mkdir -p /etc/netns/ap0
 sudo bash -c "echo \"nameserver 1.1.1.1\" > /etc/netns/ap0/resolv.conf"
-sudo bash -c "echo \"nameserver 8.8.8.8\" > /etc/netns/ap0/resolv.conf"
-sudo bash -c "echo \"nameserver 9.9.9.9\" > /etc/netns/ap0/resolv.conf"
+sudo bash -c "echo \"nameserver 8.8.8.8\" >> /etc/netns/ap0/resolv.conf"
+sudo bash -c "echo \"nameserver 9.9.9.9\" >> /etc/netns/ap0/resolv.conf"
 
 sudo ip netns add ap0
 sudo ip netns exec ap0 ip l set up lo
-sudo ip netns exec ap0 ip link add eth0 type veth peer name vap0 
+sudo ip netns exec ap0 ip link add eth0 type veth peer name vap0
 sudo ip netns exec ap0 ip l set dev vap0 netns 1
 sudo ip netns exec ap0 ip l set up dev eth0
 
 
 sudo ip l set up dev vap0
-sudo ip l set dev vap0 master br0  
+sudo ip l set dev vap0 master br0
 
 sudo ip addr add 192.168.100.1/24  brd + dev br0
 
-sudo dnsmasq -i br0 --dhcp-range=192.168.100.2,192.168.100.200,6h --dhcp-option=6,1.1.1.1,8.8.8.8 --dhcp-authoritative --bind-interfaces  -d --dhcp-leasefile=dhcp.leases > dnsmasq.out 2>&1 & echo $! > dns.pid
+sudo dnsmasq -i br0 --dhcp-range=192.168.100.2,192.168.100.200,6h --dhcp-option=6,1.1.1.1,8.8.8.8 --dhcp-authoritative --bind-interfaces  -d --dhcp-leasefile=dhcp.leases > dnsmasq.out 2>&1 & echo $! > dhcp.pid
 
 
 sudo iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -j MASQUERADE --random
@@ -33,4 +33,19 @@ sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -d 192.168.100.0/2
 
 sudo ip netns exec ap0 dhclient eth0 -v
 
-sudo ip netns exec ap0 bash
+
+sudo iw phy phy0 set netns name /var/run/netns/ap0
+
+sudo ip netns exec ap0 ip l set up dev wlan0
+
+sudo ip netns exec ap0 ip addr add 192.168.200.1/24 brd + dev wlan0
+
+sudo ip netns exec ap0 iptables -t nat -A POSTROUTING -s 192.168.200.0/24 -j MASQUERADE --random
+sudo ip netns exec ap0 iptables -A FORWARD -s 192.168.200.0/24 -o eth0 -j ACCEPT
+sudo ip netns exec ap0 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -d 192.168.200.0/24 -i eth0 -j ACCEPT
+
+sudo ip netns exec ap0 hostapd ap.conf -P hostapd.pid -B
+
+sudo ip netns exec ap0 dnsmasq -i wlan0 --dhcp-range=192.168.200.2,192.168.200.200,6h --dhcp-option=6,1.1.1.1,8.8.8.8,9.9.9.9 --dhcp-authoritative --bind-interfaces  -d --dhcp-leasefile=wlan.leases > dnsmasq_wlan.out 2>&1 & echo $! > dhcp_wlan.pid
+
+# sudo ip netns exec ap0 bash
--- destroy_ap.sh
+++ destroy_ap.sh
@@ -2,10 +2,16 @@
 
 #set -e
 
-sudo kill -2 $(cat dns.pid)
 
-rm dns.pid
+sudo kill -2 $(cat dhcp.pid)
+sudo kill -2 $(cat dhcp_wlan.pid)
+sudo kill -2 $(cat hostapd.pid)
 
+sudo rm -rf dhcp.pid
+sudo rm -rf dhcp_wlan.pid
+sudo rm -rf hostapd.pid
+
+sudo ip netns exec ap0 iw phy phy0 set netns 1
 
 sudo iptables -t nat -D POSTROUTING -s 192.168.100.0/24  -j MASQUERADE --random
 
